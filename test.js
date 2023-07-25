f9e565015226bacb8fd611aa8be67316c75a6097
zingydibaboom <lfan@tcd.ie>
2023-07-25 23:26:28 +0100

    homepage done

--- test.js
+++ test.js
@@ -1,18 +1,78 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const areas = document.querySelectorAll("area[data-tooltip]");
-  
-    areas.forEach(function(area) {
-      const tooltipText = area.dataset.tooltip;
-  
-      const tooltip = document.createElement("div");
-      tooltip.classList.add("tooltip");
-      tooltip.textContent = tooltipText;
-  
-      const areaWrapper = document.createElement("div");
-      areaWrapper.classList.add("area-tooltip");
-  
-      areaWrapper.appendChild(tooltip);
-      area.parentNode.appendChild(areaWrapper);
-    });
+const tooltips = document.querySelectorAll(".all-tooltip .tooltip");
+const fullDiv = document.querySelector("section");
+const container = document.querySelector(".container");
+let timeoutId;
+window.addEventListener("resize", contentPosition);
+window.addEventListener("DOMContentLoaded", contentPosition);
+
+function contentPosition() {
+  tooltips.forEach((tooltip) => {
+    const pin = tooltip.querySelector(".pin");
+    const content = tooltip.querySelector(".tooltip-content");
+    const arrow = tooltip.querySelector(".arrow");
+    const pinLeft = pin.offsetLeft;
+    if (pinLeft + content.offsetWidth / 2 > fullDiv.offsetWidth) {
+      const extraLeft =
+        fullDiv.offsetWidth - (pinLeft + content.offsetWidth / 2);
+      // console.log('right-conflict', tooltip)
+      content.style.left =
+        pinLeft - content.offsetWidth / 2 + extraLeft - 30 + "px";
+      content.style.top = pin.offsetTop + 30 + "px";
+    } else if (
+      pin.offsetLeft + container.offsetLeft <
+      content.offsetWidth / 2
+    ) {
+      // console.log('left conflict', pin.offsetLeft)
+      content.style.left = -container.offsetLeft + "px";
+      content.style.top = pin.offsetTop + 30 + "px";
+    } else {
+      content.style.left = pinLeft - content.offsetWidth / 2 + "px";
+      content.style.top = pin.offsetTop + 30 + "px";
+    }
+    arrow.style.left =
+      pinLeft - content.offsetLeft + pin.offsetWidth / 2 + "px";
   });
-  
+}
+tooltips.forEach((tooltip) => {
+  const pin = tooltip.querySelector(".pin");
+  const content = tooltip.querySelector(".tooltip-content");
+  pin.addEventListener("mouseover", () => {
+    tooltip.classList.add("active");
+  });
+  pin.addEventListener("mouseleave", () => {
+    timeoutId = setTimeout(() => {
+      if (!tooltip.classList.contains("content-hover")) {
+        tooltip.classList.remove("active");
+      }
+    },1500);
+  });
+  content.addEventListener("mouseover", () => {
+    clearTimeout(timeoutId);
+    tooltip.classList.add("active");
+    tooltip.classList.add("content-hover");
+  });
+  content.addEventListener("mouseleave", () => {
+    timeoutId = setTimeout(() => {
+      tooltip.classList.remove("active");
+      tooltip.classList.remove("content-hover");
+    }, 1500);
+  });
+});
+
+// *********************
+// This Code is for only the floating card in right bottom corner
+// **********************
+
+const WebCifarIcon = document.querySelector("#webCifar-icon");
+const WebCifarEl = document.querySelector("#webCifar");
+const close = WebCifarEl.querySelector(".close");
+const youtubeLink = document.querySelector(".youtubeLink");
+
+WebCifarIcon.addEventListener("click", () => {
+  WebCifarEl.classList.add("active");
+});
+close.addEventListener("click", () => {
+  WebCifarEl.classList.remove("active");
+});
+
+youtubeLink.setAttribute("href", "https://youtu.be/e_jEquJo7y8");
